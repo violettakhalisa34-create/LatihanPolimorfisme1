1ba70fd4029ca3cba591940b577e28365f17f78e
violettakhalisa34-create <violettakhalisa34@gmail.com>
2026-04-08 14:53:05 +0700

    Update Program.cs
    tambahan

--- LatihanPolimorfisme1/Program.cs
+++ LatihanPolimorfisme1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+ImageProcessor imageProcessor = new ImageProcessor();
+
+imageProcessor.Resize(-20);
+imageProcessor.Resize(80);
+imageProcessor.Resize(150);
+imageProcessor.Resize(100);
+
+class ImageProcessor
+{
+    public void Resize(int persentase)
+    {
+        if(persentase <= 0)
+        {
+            Console.WriteLine("Eror! persentase tidak boleh nol(0) atau negatif");
+        }
+        else if (persentase < 100)
+        {
+            Console.WriteLine($"Mengecilkan gambar menjadi {persentase}%");
+        }
+        else if (persentase > 100)
+        {
+            Console.WriteLine($"Memperbesar gambar menjadi {persentase}%");
+        }
+        else
+        {
+            Console.WriteLine("ukuran tetap (100%). tidak ada perubahan");
+        }
+    }
+    public void Resize(int lebar, int tinggi)
+    {
+        Console.WriteLine($"Mengubah ukuran gambar menjadi {lebar}px X {tinggi}px");
+    }
+    public void Resize(string ukuran_kertas)
+    {
+        Console.WriteLine($"Menyesuaikan ke format kertas {ukuran_kertas}");
+    }
+}
